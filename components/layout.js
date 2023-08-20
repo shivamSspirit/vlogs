6ea20d6f34fa733edf0426c547f40ed566778b87
shivam <shivam.spirite3@outlook.com>
2023-08-20 11:25:11 +0530

    change og file

--- components/layout.js
+++ components/layout.js
@@ -21,7 +21,7 @@ export default function Layout({children ,home, postData}){
                property="og:image"
                content={`https://og-image.vercel.app/${encodeURI(
                 siteTitle
-              )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+              )}.https://imgur.com/ptBonMb`}
             />
              <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
